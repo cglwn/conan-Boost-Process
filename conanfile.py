a3cc1c2f54abc6bd7f1f517c5e0e706ef8db5023
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:18 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,8 @@ from conans import ConanFile, tools, os
 class BoostProcessConan(ConanFile):
     name = "Boost.Process"
     version = "1.64.0"
-    generators = "txt"
-    url = "https://github.com/boostorg/process"
+    url = "https://github.com/bincrafters/conan-boost-process"
+    source_url = "https://github.com/boostorg/process"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "process"
@@ -26,7 +26,7 @@ class BoostProcessConan(ConanFile):
                       
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
+                 .format(self.version, self.source_url))
 
     def package(self):
         include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
