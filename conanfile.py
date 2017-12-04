588cf167f409520b289ce513b9f51ee46dad7644
Rene Rivera <grafikrobot@gmail.com>
2017-12-03 23:37:29 -0600

    Next generation of Boost packages.

--- conanfile.py
+++ conanfile.py
@@ -1,42 +1,48 @@
 from conans import ConanFile, tools
-import os
+
 
 class BoostProcessConan(ConanFile):
     name = "Boost.Process"
     version = "1.65.1"
-    short_paths = True
     url = "https://github.com/bincrafters/conan-boost-process"
     description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
+
+    requires = \
+        "Boost.Algorithm/1.65.1@bincrafters/testing", \
+        "Boost.Asio/1.65.1@bincrafters/testing", \
+        "Boost.Config/1.65.1@bincrafters/testing", \
+        "Boost.Core/1.65.1@bincrafters/testing", \
+        "Boost.Filesystem/1.65.1@bincrafters/testing", \
+        "Boost.Fusion/1.65.1@bincrafters/testing", \
+        "Boost.Iterator/1.65.1@bincrafters/testing", \
+        "Boost.Move/1.65.1@bincrafters/testing", \
+        "Boost.Optional/1.65.1@bincrafters/testing", \
+        "Boost.System/1.65.1@bincrafters/testing", \
+        "Boost.Tokenizer/1.65.1@bincrafters/testing", \
+        "Boost.Type_Index/1.65.1@bincrafters/testing", \
+        "Boost.Winapi/1.65.1@bincrafters/testing"
+
     lib_short_names = ["process"]
-    requires =  "Boost.Algorithm/1.65.1@bincrafters/testing", \
-                      "Boost.Asio/1.65.1@bincrafters/testing", \
-                      "Boost.Config/1.65.1@bincrafters/testing", \
-                      "Boost.Core/1.65.1@bincrafters/testing", \
-                      "Boost.Filesystem/1.65.1@bincrafters/testing", \
-                      "Boost.Fusion/1.65.1@bincrafters/testing", \
-                      "Boost.Iterator/1.65.1@bincrafters/testing", \
-                      "Boost.Move/1.65.1@bincrafters/testing", \
-                      "Boost.Optional/1.65.1@bincrafters/testing", \
-                      "Boost.System/1.65.1@bincrafters/testing", \
-                      "Boost.Tokenizer/1.65.1@bincrafters/testing", \
-                      "Boost.Type_Index/1.65.1@bincrafters/testing", \
-                      "Boost.Winapi/1.65.1@bincrafters/testing"
+    is_header_only = True
 
-                      #algorithm9 asio14 config0 core2 filesystem8 fusion5 iterator5 move3 optional5 system3 tokenizer6 type_index5 winapi1
-                      
-    def source(self):
-        boostorg_github = "https://github.com/boostorg"
-        archive_name = "boost-" + self.version  
-        for lib_short_name in self.lib_short_names:
-            tools.get("{0}/{1}/archive/{2}.tar.gz"
-                .format(boostorg_github, lib_short_name, archive_name))
-            os.rename(lib_short_name + "-" + archive_name, lib_short_name)
+    # BEGIN
 
-    def package(self):
-        for lib_short_name in self.lib_short_names:
-            include_dir = os.path.join(lib_short_name, "include")
-            self.copy(pattern="*", dst="include", src=include_dir)		
+    short_paths = True
+    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
 
     def package_id(self):
-        self.info.header_only()
+        if self.is_header_only:
+            self.info.header_only()
+
+    @property
+    def env(self):
+        try:
+            with tools.pythonpath(super(self.__class__, self)):
+                import boostgenerator # pylint: disable=F0401
+                boostgenerator.BoostConanFile(self)
+        except:
+            pass
+        return super(self.__class__, self).env
+
+    # END
